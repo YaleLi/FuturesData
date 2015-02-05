4e6461b35817dda0432dc530046243e0ca27a18e
YaleLi <yalelee_tj@hotmail.com>
2015-02-05 23:58:26 +0800

    Add Unittest

--- DataCrawler/DataCrawler.cs
+++ DataCrawler/DataCrawler.cs
@@ -1,29 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DataCrawler
 {
-    public class DataCrawler
+    using System;
+    using System.Configuration;
+    using System.Globalization;
+    using System.IO;
+    using System.Net;
+    using System.Text;
+
+    public abstract class DataCrawler
     {
-        public void PullData(DateTime start, DateTime end, Func<string, DateTime> dataHandler)
+        protected Encoding ContentEncoding { get; set; }
+        protected string PullData(DateTime date, string url)
+        {
+            if(string.IsNullOrEmpty(url))
+            {
+                return "";
+            }
+
+            string content = null;
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var reader = new StreamReader(response.GetResponseStream(), ContentEncoding);
+
+            return content;
+        }
+
+        abstract protected string BuildUrl();
+        public void PullData(DateTime start, DateTime end, Action<string, DateTime> dataHandler)
         {
             DateTime endTime = end;
-            if(end.Hour < 18)
+            if(end.Hour < Int32.Parse(ConfigurationManager.AppSettings["DataPublishTime"], NumberStyles.Any))
             {
-
+                endTime = endTime.AddDays(-1);
             }
             if(end.CompareTo(start) < 0)
             {
                 return;
             }
 
-            if(null != dataHandler)
+            DateTime date = start;
+            string content = "";
+            while(date.CompareTo(endTime) <= 0)
             {
-                dataHandler()
+                content = PullData(date, BuildUrl());
+                if (null != dataHandler && !string.IsNullOrEmpty(content))
+                {
+                    dataHandler(content, date);
+                }
             }
+
         }
     }
 }
---
+++ FuturesDataTest/DataCrawlerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FuturesDataTest
+{
+    [TestClass]
+    public class DataCrawlerTest
+    {
+        [TestMethod]
+        public void DownloadShfeDealerInfoTest()
+        {
+        }
+
+        [TestMethod]
+        public void DownloadShfeTransactionInfoTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void DownloadDceDealerCommodityInfoTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void DownloadDceDealerContractInfoTest()
+        {
+
+        }
+    }
+}
